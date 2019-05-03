8fdadb302967faba67e1496d078e4af0025535f3
Salome Kariuki <salomekariuki2020@u.northwestern.edu>
2019-05-03 13:36:38 -0500

    lab5 complete

---
+++ labs/lab05/03-color-mixer-optional/js/index.js
@@ -0,0 +1,18 @@
+// Challenge 1: Can you use the three text boxes to change the background color?
+// Challenge 2: Can you get the background to change anytime the user updates the
+//              number (so that clicking the button is not necessary)?
+
+const redElement = document.getElementById("red");
+const greenElement = document.getElementById("green");
+const blueElement = document.getElementById("blue");
+
+const updateColor = () => {
+    console.log('Use the redElement, greenElement, and blueElement to set the background!')
+    // change this so that it's not hard-coded:
+    var red = document.getElementById("red").value;
+    var green = document.getElementById("green").value;
+    var blue = document.getElementById("blue").value;
+    document.querySelector('body').style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue +')';
+}
+
+document.querySelector('button').onclick = updateColor;
