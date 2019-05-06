911243a3f268eed98d9c0691f0637ad0fe377dea
Salome Kariuki <salomekariuki2020@u.northwestern.edu>
2019-05-06 13:43:36 -0500

    completed

---
+++ hw05/01-unique-messages/js/index.js
@@ -0,0 +1,15 @@
+const names = [
+    "Jane", "Brenda", "Wanda", "Maria", "Jasper",
+    "John", "Malik", "Arjun", "Larry", "Curly", "Moe"
+];
+
+
+// use a for / of loop to loop through each element
+// of the names array and output a message to the screen:
+// document.write("Welcome, " + names[0] + "! Make yourself comfortable.<br>");
+// document.write("Welcome, " + names[1] + "! Make yourself comfortable.<br>");
+// document.write("Welcome, " + names[2] + "! Make yourself comfortable.<br>");
+for(var i = 0; i<names.length; i++)
+{
+    document.write("Welcome, " + names[i] + "! Make yourself comfortable.<br>");
+}
